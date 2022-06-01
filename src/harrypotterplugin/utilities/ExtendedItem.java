5a5ea2c74819d145c8d74d17157ed699a625b070
Der-Zauberer <and.som282@gmail.com>
2022-06-01 15:02:52 +0200

    Improved ExtendedItem

--- src/harrypotterplugin/utilities/ExtendedItem.java
+++ src/harrypotterplugin/utilities/ExtendedItem.java
@@ -1,9 +1,12 @@
 package harrypotterplugin.utilities;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
-
 import harrypotterplugin.actions.ItemInteractAction;
 import harrypotterplugin.actions.ItemLeftClickAction;
 import harrypotterplugin.actions.ItemRightClickAction;
@@ -19,6 +22,45 @@ public class ExtendedItem extends ItemStack {
 		ItemMeta itemMeta = getItemMeta();
 		itemMeta.setDisplayName(displayName);
 		itemMeta.setCustomModelData(customModelData);
+		setItemMeta(itemMeta);
+	}
+	
+	public void setCustomModelData(int customModelData) {
+		ItemMeta itemMeta = getItemMeta();
+		itemMeta.setCustomModelData(customModelData);
+		setItemMeta(itemMeta);
+	}
+	
+	public void setLore(String string) {
+		String[] list = string.split("\n");
+		List<String> lore = new ArrayList<>();
+		for (String line : list) lore.add(line);
+		ItemMeta itemMeta = getItemMeta();
+		itemMeta.setLore(lore);
+		setItemMeta(itemMeta);
+	}
+	
+	public void setLore(List<String> lore) {
+		ItemMeta itemMeta = getItemMeta();
+		itemMeta.setLore(lore);
+		setItemMeta(itemMeta);
+	}
+	
+	public void addEnchantment(Enchantment enchantment, int level) {
+		ItemMeta itemMeta = getItemMeta();
+		itemMeta.addEnchant(enchantment, level, true);
+		setItemMeta(itemMeta);
+	}
+	
+	public void addItemFlag(ItemFlag flag) {
+		ItemMeta itemMeta = getItemMeta();
+		itemMeta.addItemFlags(flag);
+		setItemMeta(itemMeta);
+	}
+	
+	public void removeItemFlag(ItemFlag flag) {
+		ItemMeta itemMeta = getItemMeta();
+		itemMeta.removeItemFlags(flag);
 		setItemMeta(itemMeta);
 	}
 	
